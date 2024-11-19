402027cc69d080d4fbe6cc3791f31fcfba6b0d89
Mamun Mohamed <mohamedmamun85@gmail.com>
2024-11-19 15:30:16 +0100

    Remove @Controller and add @RestController

--- src/main/java/org/mamunmohamed/schwarz/infrastructure/api/CouponResource.java
+++ src/main/java/org/mamunmohamed/schwarz/infrastructure/api/CouponResource.java
@@ -14,17 +14,13 @@ import org.mamunmohamed.schwarz.infrastructure.mapper.BasketMapper;
 import org.mamunmohamed.schwarz.infrastructure.mapper.CouponMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @Slf4j
